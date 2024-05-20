13b80174020722ac1fbaf00507aa6a30adecd50f
syronesdreamland <116932673+syronesdreamland@users.noreply.github.com>
2024-05-20 23:56:53 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,36 @@
+<!-- App.vue -->
+<template>
+	<main class="app">
+	  <header class="header">
+		<nav class="menu">
+		  <button @click="switchToPost" class="btn btn-primary" style="color: green; margin-right: 10px; padding: 5px 5px; border-radius: 5px; font-size: 16px; font-weight: bold;" :class="{ active: activeMenu === 'post' }">Post</button>
+		  <button @click="switchToTodos" class="btn btn-primary" style="color: green; margin-right: 10px; padding: 5px 5px; border-radius: 5px; font-size: 16px; font-weight: bold;" :class="{ active: activeMenu === 'todos' }">Todos</button>
+		</nav>
+	  </header>
+  
+	  <todo v-if="activeMenu === 'todos'" />
+  
+	  <post v-if="activeMenu === 'post'" />
+	</main>
+  </template>
+  
+  <script setup>
+  import Todo from './Todo.vue'
+  import Post from './Post.vue'
+  import { ref } from 'vue'
+  
+  const activeMenu = ref('todos')
+  
+  const switchToTodos = () => {
+	activeMenu.value = 'todos'
+  }
+  
+  const switchToPost = () => {
+	activeMenu.value = 'post'
+  }
+  </script>
+  
+  <style scoped>
+  /* Tambahkan styling CSS sesuai kebutuhan Anda */
+  </style>
+  
